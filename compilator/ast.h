9bc6bb86ac381848dc73d9d8dfe430ee68981909
eLruloSz <130678389+eLruloSz@users.noreply.github.com>
2025-06-11 23:15:56 -0400

    subir compilator general
    espero haberla subido bien porque no sé mucho

---
+++ compilator/ast.h
@@ -0,0 +1,48 @@
+#ifndef AST_H
+#define AST_H
+
+#include <string>
+
+enum NodeType {
+    NODE_INT,
+    NODE_ID,
+    NODE_ASSIGN,
+    NODE_PRINT,
+    NODE_IF,
+    NODE_WHILE,
+    NODE_BINOP,
+    NODE_SEQ
+};
+
+struct AST {
+    NodeType type;
+    union {
+        int intval;
+        char* id;
+        struct {
+            AST* left;
+            AST* right;
+        } bin;
+        struct {
+            AST* cond;
+            AST* then_branch;
+            AST* else_branch;
+        } ctrl;
+        struct {
+            AST* first;
+            AST* second;
+        } seq;
+    } data;
+};
+
+AST* make_int(int val);
+AST* make_id(const char* id);
+AST* make_assign(AST* id, AST* val);
+AST* make_print(AST* expr);
+AST* make_if(AST* cond, AST* then_b, AST* else_b);
+AST* make_while(AST* cond, AST* body);
+AST* make_binop(int op, AST* lhs, AST* rhs);
+AST* make_seq(AST* first, AST* second);
+void print_ast(AST* tree, int indent);
+
+#endif
